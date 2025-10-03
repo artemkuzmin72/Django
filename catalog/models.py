6fcd84e9b22c231fb907a579c2e73cb1f0a69151
artemkuzmin <artemkuzmin77@icloud.com>
2025-10-03 12:32:55 +0300

    Доработки

--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,13 @@ class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
@@ -11,5 +18,13 @@ class Product(models.Model):
     pics = models.ImageField(upload_to='images/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['last_name']
