0fde788a765c05710ddff6079c070d62577caabe
artemkuzmin <artemkuzmin77@icloud.com>
2025-10-27 00:51:30 +0300

    Cache

--- catalog/services.py
+++ catalog/services.py
@@ -1,4 +1,4 @@
 from .models import Product
 
 def get_products_by_category(category_id):
-    return Product.objects.filter(category_id=category_id, is_published=True).select_related('category', 'owner')
+    return Product.objects.filter(category__id=category_id, is_published=True).select_related('category', 'owner')
